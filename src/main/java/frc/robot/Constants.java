9a79d0845c3e4be135b5daaab49538525f900e83
fielia <rit.g.garg@gmail.com>
2022-04-19 11:20:33 -0400

    added note about Constants.java organization, for the future

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,4 +12,7 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
+/**
+ * TODO: PROGRAMMER NOTE: organize constants alphabetically, first by file name then by variable name
+ */
 public final class Constants {}
